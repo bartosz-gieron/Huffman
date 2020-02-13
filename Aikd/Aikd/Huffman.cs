3c1111a372a233e51cd1c99b4017a496e507a45e
bartosz-gieron <58327656+bartosz-gieron@users.noreply.github.com>
2020-02-13 12:33:30 +0100

    Add files via upload

---
+++ Aikd/Aikd/Huffman.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Aikd
+{
+    class Huffman
+    {
+        private string FileText;
+        private readonly string FileName;
+        private List<Node> Nodes;
+        private List<Node> Tree;
+        private List<Node> Code;
+        public Huffman(string FileName)
+        {
+            this.FileName = FileName;
+            Tree = new List<Node>();
+            Nodes = new List<Node>();
+            Code = new List<Node>();
+            if(FileOpen())
+            {
+                Console.WriteLine($"zawartosc pliku:  {FileText}\n\n");
+                GenerateNodes();
+                Tree = Nodes;
+                GenerateTree();
+                GenerateCode();
+
+                foreach(var letter in FileText)
+                {
+                    Console.Write(Code.First(a => a.Char == letter).Code + " ");
+                }
+            }
+        }
+
+        private void GenerateTree()
+        {
+            while (Tree.Count() != 1)
+            {
+                Tree = Tree.OrderBy(t => t.Count).ToList();
+
+                Tree[0].Dir = "LEWO";
+                Tree[1].Dir = "PRAWO";
+
+                var newNode = new Node(null);
+                newNode.Left = Tree[0];
+                newNode.Right = Tree[1];
+                Tree.Add(newNode);
+
+                Tree.RemoveAt(0);
+                Tree.RemoveAt(0);
+            }
+        }
+
+        private void GenerateCode(Node node = null, string code="")
+        {
+            if (node == null)
+                node = Tree.First();
+
+            if (node.Char != '+')
+            {
+                node.Code = code;
+                Code.Add(node);
+            }
+
+            if (node.Left != null)
+                GenerateCode(node.Left, code + "0");
+            if (node.Right != null)
+                GenerateCode(node.Right, code + "1");
+        }
+
+        private void GenerateNodes()
+        {
+            foreach(var letter in FileText)
+            {
+                if (Nodes.All(a => a.Char != letter))
+                    Nodes.Add(new Node(letter));
+                else
+                    Nodes.First(b => b.Char == letter).CountAdd();
+            }
+        }
+
+        public Boolean FileOpen()
+        {
+            string text = "";
+            try
+            {
+                using (var sr = new StreamReader(FileName))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        text += line.ToUpper();
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"nie znaleziono pliku({e.Message})");
+                return false;
+            }
+            FileText = text;
+            return true;
+        }
+    }
+}
---
+++ Aikd/Aikd/Node.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aikd
+{
+    class Node
+    { 
+        public char? Char { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public string Code { get; set; }
+        public string Dir { get; set; }
+
+        private int _Count;
+        public int Count
+        {
+            get
+            {
+                if (Left != null || Right != null)
+                {
+
+                    _Count = 0;
+                    if (Left != null)
+                        _Count += Left.Count;
+
+                    if (Right != null)
+                        _Count += Right.Count;
+                }
+                return _Count;
+            }
+    }
+
+    public Node(char? newChar)
+    {
+        Code = "";
+        Char = newChar;
+        _Count = 1;
+    }
+
+    public void CountAdd()
+    {
+        _Count++;
+    }
+}
+}
